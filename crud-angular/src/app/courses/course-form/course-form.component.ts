a7e16d1c1ebc9450f5b5b2a3d82b5a9978111894
Leonardo Padilha <leoprsouza@hotmail.com>
2022-09-08 01:00:38 -0300

    implementacao metodo salvar

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,44 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
 
 import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.scss']
+  styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder,
-     private service: CoursesService,
-     private snackBar: MatSnackBar) {
-
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar,
+    private location: Location
+  ) {
     this.form = this.formBuilder.group({
       name: [null],
-      category: [null]
-    })
-
-   }
-
-  ngOnInit(): void {
-
+      category: [null],
+    });
   }
 
-  onSubmit(){
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.log(this.form.value)
     this.service.save(this.form.value)
-    .subscribe(result => console.log(result), error => this.onError());
+    .subscribe(
+      (result) => this.onSuccess,
+      (error) => this.onError()
+    );
   }
 
-  onCancel(){
-
+  onCancel() {
+    this.location.back;
   }
 
-  private  onError(){
-    this.snackBar.open('Erro ao salvar curso!', '', {duration: 5000});
+  private onError() {
+    this.snackBar.open('Erro ao salvar curso!', '', { duration: 5000 });
+  }
+
+  private onSuccess() {
+    this.snackBar.open('Curso salvo com sucesso!', '', { duration: 5000 });
+    this.onCancel();
   }
 
 }
